0f99fc2e793f955b25831cdb5108c80fddff7d76
Reddyt_Shokker <jahir_godinez@outlook.com>
2025-05-08 17:03:31 -0600

    Agregar archivos de proyecto.

---
+++ js/cambiarImagenes.js
@@ -0,0 +1,32 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const boton = document.getElementById("cambiarBtn");
+    const imagenes = document.querySelectorAll(".icono-tec");
+
+    const originales = [
+        "img/htmlNormal.png",
+        "img/cssNormal.png",
+        "img/jsNormal.png",
+        "img/pythonNormal.png",
+        "img/javaNormal.png"
+    ];
+
+    const alternativas = [
+        "img/html.png",
+        "img/css.png",
+        "img/js.png",
+        "img/python.png",
+        "img/java.png"
+    ];
+
+    let usandoAlternativas = false;
+
+    boton.addEventListener("click", function () {
+        imagenes.forEach((img, i) => {
+            img.src = usandoAlternativas ? originales[i] : alternativas[i];
+        });
+
+        usandoAlternativas = !usandoAlternativas;
+
+        boton.textContent = usandoAlternativas ? "No UwU" : "¿UwU?";
+    });
+});
---
+++ js/maquina.js
@@ -0,0 +1,12 @@
+﻿const texto = "De ideas a realidad";
+let i = 0;
+
+function escribir() {
+    if (i < texto.length) {
+        document.getElementById("frase").textContent += texto.charAt(i);
+        i++;
+        setTimeout(escribir, 60);
+    }
+}
+
+window.addEventListener("load", escribir);
